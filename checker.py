71ea073012d24d6a70a3bad2573f6708939f7e74
nasus <77024697+nasusL@users.noreply.github.com>
2021-12-03 22:04:53 +0100

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,18 +1,14 @@
-import os, time, random, string, requests
-from discord_webhook import DiscordWebhook
+import time, random, string, requests
 
-webhook_url = "" # Create a webhook and enter the URL here
-os.system('cls' if os.name == 'nt' else 'clear')
-input("\nClassic Discord Nitro Checker w/ webhooks by nasus#5311 | MIT License\nThis tool might be slow due to unavoidable and seemingly exponential ratelimits\nMake sure there are codes in your codes.txt file\n")
+input("\nClassic Discord Nitro Checker by nasus\nTool will be slow due to ratelimits | Make sure there are codes in your codes.txt file\n")
 while True:
     with open("codes.txt") as f:
         code = f.readline().rstrip()
     url = requests.get(f"https://discordapp.com/api/v9/entitlements/gift-codes/{code}?with_application=false&with_subscription_plan=true")
     if url.status_code == 200:
-        print(f"Valid Code | {code}")
-        DiscordWebhook(url=webhook, rate_limit_retry=True, content=f"Valid code detected! @everyone discord.gift/{code}").execute()
+        quit(f"[Valid] {code}")
     elif url.status_code == 404:
-        print(f"Invalid Code | {code}")
+        print(f"[Invalid] Code | {code}")
         # remove line once used
         with open('codes.txt', 'r') as f:
             data = f.read().splitlines(True)
@@ -20,7 +16,5 @@ while True:
             t.writelines(data[1:])
     elif url.status_code == 429:
         rate = int(url.json()['retry_after']) + 1
-        print(f"Ratelimited! {round(rate, 2)}")
+        print(f"Ratelimited! {round(rate, 2)}s")
         time.sleep(rate)
-    else:
-        print(f"Invalid Error! | Status code {url.status_code}")
